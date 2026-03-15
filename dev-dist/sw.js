4a556650ca4b6abf89db0021f5eedcf008fd68ac
Eternal-Allies <eternalallies.au@gmail.com>
2026-03-15 21:28:33 +0530

    new

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.ifkc8rf2clc"
+    "revision": "0.3aqbr2k9fv4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
